5beaa88ef21f54afe499802d747761b9da70dc27
peichieh <lpg0718bc@gmail.com>
2019-05-25 21:00:06 +0800

    create xcode project

---
+++ FinalProject/FinalProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FinalProject
+//
+//  Created by lo peichieh on 2019/5/25.
+//  Copyright © 2019 lo peichieh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FinalProject/FinalProject/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  FinalProject
+//
+//  Created by lo peichieh on 2019/5/25.
+//  Copyright © 2019 lo peichieh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
